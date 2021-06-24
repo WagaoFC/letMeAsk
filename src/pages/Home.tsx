8c790dbba225917a473a61b2547c017bbeff76ff
WagaoFC <wagnerferreira_07@hotmail.com>
2021-06-24 00:13:46 -0300

    criacao das rotas home e room news

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,51 @@
-import illustrationImg from '../assets/images/illustration.svg'
-import logoImg from '../assets/images/logo.svg'
-import googleIconImg from '../assets/images/google-icon.svg'
+import { useHistory } from 'react-router-dom'
+
+import { auth, firebase } from '../services/firebase'
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+import { Button } from '../components/Button';
+
+import '../styles/auth.scss';
 
 export function Home() {
+    const history = useHistory();
+
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+        auth.signInWithPopup(provider).then(result => {
+            console.log(result);
+        })
+
+        // history.push('/rooms/new');
+    }
+
     return(
-        <div>
+        <div id="page-auth">
             <aside>
                 <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas"/>
                 <strong>Crie salas de Q&amp;A ao-vivo</strong>
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <div>
+                <div className="main-content">
                     <img src={logoImg} alt="Letmeask"/>
-                    <button>
+                    <button onClick={handleCreateRoom} className="create-room">
                         <img src={googleIconImg} alt="Logo do Google"/>
                         Crie sua sala com o Google
                     </button>
-                    <div>
-                        ou entre em uma salaaaaa
+                    <div className="separator">
+                        ou entre em uma sala
                     </div>
                     <form>
                         <input
                             type="text"
                             placeholder="Digite o código da sala"
                         />
-                        <button type="submit">
+                        <Button type="submit">
                             Entrar na sala
-                        </button>
+                        </Button>
                     </form>
                 </div>
             </main>
